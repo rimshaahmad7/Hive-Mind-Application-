1da522051bc6bbd1e3ece626b1a9a7a2bcbc0581
Jasmine-Haque <144148481+Jasmine-Haque@users.noreply.github.com>
2024-05-04 19:20:34 +0530

    final

--- Hive Mind/Games/QUIZZ.1/highscores.js
+++
@@ -1,8 +0,0 @@
-const highScoreList = document.getElementById('highScoreList');
-const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
-
-highScoreList.innerHTML =
-    highScores.map(score => {
-    return`<li class="high-score">${score.name} - ${score.score}</li>`;
-})
-.join("");
--- Hive Mind/Games/Quizz/userinfo.js
+++
@@ -1,3 +0,0 @@
-let user_name = sessionStorage.getItem("name");
-
-document.querySelector("span.name").innerHTML = user_name;
